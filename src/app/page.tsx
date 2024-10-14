227c00551dd82e4d62edc889b3cea7b612a64c57
Luis Felipe Salvarani <55268395+LuisFelipeSalvarani@users.noreply.github.com>
2024-10-14 16:22:21 -0300

    feat: layout básico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { HomeIcon, Library, Search } from 'lucide-react'
+
 export default function Home() {
   return (
-    <main>
-      <h1>Spotify</h1>
-    </main>
+    <div className="flex h-screen flex-col">
+      <div className="flex flex-1">
+        <aside className="w-72 bg-zinc-950 p-6">Sidebar</aside>
+        <main className="flex-1 p-6">main</main>
+      </div>
+      <footer className="border-zinc-700 border-t bg-zinc-800 p-6">
+        footer
+      </footer>
+    </div>
   )
 }
